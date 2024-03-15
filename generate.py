138688cd2b438e9dd12ff7602391d9ad80ab2a11
Ayus Chatterjee <144832717+ayusc@users.noreply.github.com>
2024-03-15 10:59:04 +0530

    major code improvement

--- generate.py
+++ generate.py
@@ -4,13 +4,14 @@ My modifications to the given code:
 	1) Include numbers and special characters in passwords
 	2) Provide an option for all passwords to be of same length
         3) Add checks for invalid inputs 
+	4) Provide options for various combinations of passwords
 	
 ~ Ayus Chatterjee	
 """
 
 import random
 
-def generatePassword(pwlength, all_same_length=False):
+def generatePassword(pwlength, char_type):
     string = "abcdefghijklmnopqrstuvwxyz"
     special_characters = "!@#$%&"
     numbers = "0123456789"
@@ -19,13 +20,20 @@ def generatePassword(pwlength, all_same_length=False):
     for length in pwlength:
         password = ""
         for _ in range(length):
-            char_type = random.choice([1, 2, 3])  # 1: lowercase letter, 2: special character, 3: number
             if char_type == 1:
                 password += random.choice(string)
             elif char_type == 2:
-                password += random.choice(special_characters)
-            else:
                 password += random.choice(numbers)
+            elif char_type == 3:
+                password += random.choice(string + special_characters)
+            elif char_type == 4:
+                password += random.choice(numbers + special_characters)
+            elif char_type == 5:
+                password += random.choice(string + numbers)
+            elif char_type == 6:
+                password += random.choice(string + numbers + special_characters)
+            else:
+                password += random.choice(string + numbers + special_characters)
         
         password = replaceWithUppercaseLetter(password)
         passwords.append(password) 
@@ -48,6 +56,24 @@ def main():
         print("Generating " + str(numPasswords) + " passwords")
         
         while True:
+            choice = input("Select the type of characters for the passwords:\n"
+                           "1. Entirely alphabetic\n"
+                           "2. Entirely numeric\n"
+                           "3. Alphabet + Special Characters\n"
+                           "4. Number + Special Characters\n"
+                           "5. Alphabet + Numbers\n"
+                           "6. Alphabet + Numbers + Special Characters\n"
+                           "Enter your choice (1/2/3/4/5/6): ").strip()
+            try:
+                char_type = int(choice)
+                if char_type not in [1, 2, 3, 4, 5, 6]:
+                    print("Invalid choice. Please enter a number between 1 and 6.\n")
+                else:
+                    break
+            except ValueError:
+                print("Invalid input. Please enter a number.\n")
+
+        while True:
             choice = input("Do you want all passwords to be of the same length? (yes/y/no/n): ").lower().strip()
             if choice in ['yes', 'y']:
                 all_same_length = True
@@ -56,7 +82,7 @@ def main():
                 all_same_length = False
                 break
             else:
-                print("Invalid choice. Please enter 'yes', 'no' or 'y', 'n'")
+                print("Invalid choice. Please enter 'yes', 'no' or 'y', 'n'\n")
 
         if all_same_length:
             while True:
@@ -84,7 +110,7 @@ def main():
                     except ValueError:
                         print("Please enter a valid integer for the password length.")
         
-        Passwords = generatePassword(passwordLengths, all_same_length)
+        Passwords = generatePassword(passwordLengths, char_type)
 
         for i, password in enumerate(Passwords):
             print("Password #" + str(i+1) + " = " + password)
@@ -92,4 +118,5 @@ def main():
     except ValueError:
         print("Please enter a valid integer for the number of passwords.")
 
-main()
+
+main()		    
